f37fde5e67c95a68ca370b32305e13ffc2da9547
AJ Speller <ajspeller@gmail.com>
2020-01-18 17:48:16 -0500

    Setup routing for elements and collections

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,11 +1,16 @@
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'elements',
+    component: ElementsHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ElementsRoutingModule { }
+export class ElementsRoutingModule {}
